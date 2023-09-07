211d7bc01aa483d43391dfcf8e50ba4c2302de2d
shashi1049 <shashi290396@gmail.com>
2023-09-08 00:48:20 +0530

    problem 1

---
+++ GoldiLocksAssignment/src/main/java/com/goldilocks/DecimalToBinary.java
@@ -0,0 +1,26 @@
+package com.goldilocks;
+
+public class DecimalToBinary {
+	public static void main(String[] args) {
+		String decimal = "10";
+		String binary = toBinary(decimal);
+		
+		System.out.println("Binary form : " +binary );
+	}
+
+	private static String toBinary(String decimal) {
+		// TODO Auto-generated method stub
+		int decNum = Integer.parseInt(decimal);
+		StringBuilder bin = new StringBuilder();
+		
+		while(decNum > 0) {
+			
+			int rem = decNum % 2;
+			bin.append(rem);
+			decNum /= 2;	
+		}
+		
+		return bin.reverse().toString();
+	}
+
+}
---
+++ GoldiLocksAssignment/src/main/java/com/goldilocks/GoldiLocksAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.goldilocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldiLocksAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldiLocksAssignmentApplication.class, args);
+	}
+
+}
